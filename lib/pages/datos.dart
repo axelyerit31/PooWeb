dbbddd63a35bfcc09dee41cb1311b62e5d6d9076
elmiki1516 <eseg.15.16@gmail.com>
2021-02-02 21:07:38 -0500

    Merge branch 'master' of https://github.com/axelyerit31/PooWeb

--- lib/pages/datos.dart
+++ lib/pages/datos.dart
@@ -1,4 +1,3 @@
-
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
@@ -17,7 +16,7 @@ var datosPlanes = [];
 var datosEspecialidades = [];
 var listaEspecialidades = [];
 
-final url = "http://192.168.18.3/PooWeb/";
+final url = "http://192.168.1.38/pooweb/";
 
 //URL para obtener datos de MySql
 final urlUsuarios = url + "obtenerPacientesUsuarios.php";
@@ -26,8 +25,8 @@ final urlObtenerPaciente = url + "obtenerPaciente.php";
 final urlObtenerPersonal = url + "obtenerPersonal.php";
 final urlObtenerAdmin = url + "obtenerAdmin.php";
 final urlObtenerPlanes = url + "obtenerPlanesDentales.php";
-final urlObtenerCitas  = url + "obtenerCitas.php";
-final urlObtenerCitasLista  = url + "obtenerCitasLista.php";
+final urlObtenerCitas = url + "obtenerCitas.php";
+final urlObtenerCitasLista = url + "obtenerCitasLista.php";
 final urlObtenerEspecialidades = url + "obtenerEspecialidades.php";
 
 //URL para insertar datos en MySql
@@ -45,179 +44,168 @@ final urlBorrarPacienteUsuario = url + "borrarPaciente.php";
 final urlEditarEspecialidad = url + "editarEspecialidad.php";
 final urlActualizarPlan = url + "actualizarPlan.php";
 
-
 //Funciones para obtener datos de MySql
 
-Future<List> obtenerUsuarios() async{
+Future<List> obtenerUsuarios() async {
   final resp = await http.get(urlUsuarios);
   return jsonDecode(resp.body);
 }
 
-Future<List> obtenerPacienteFicha(String identificador) async{
-  final resp = await http.post(urlObtenerPaciente, body: {
-    "dni": identificador
-  });
+Future<List> obtenerPacienteFicha(String identificador) async {
+  final resp =
+      await http.post(urlObtenerPaciente, body: {"dni": identificador});
 
   var resultado = jsonDecode(resp.body);
 
   return resultado;
 }
 
-Future<Map> obtenerCliente(String identificador) async{
-  final resp = await http.post(urlObtenerCliente, body: {
-    "dni": identificador
-  });
+Future<Map> obtenerCliente(String identificador) async {
+  final resp = await http.post(urlObtenerCliente, body: {"dni": identificador});
 
   var resultado = jsonDecode(resp.body)[0];
-  
+
   return resultado;
 }
 
-void obtenerPaciente(String identificador) async{
-  final resp = await http.post(urlObtenerPaciente, body: {
-    "dni": identificador
-  });
+void obtenerPaciente(String identificador) async {
+  final resp =
+      await http.post(urlObtenerPaciente, body: {"dni": identificador});
 
   var resultado = jsonDecode(resp.body);
-  
+
   datosPersonales = {
-    "dni" : resultado[0]["dni_usu"],
-    "nombres" : resultado[0]["nombres_usu"],
-    "apellidos" : resultado[0]["apellidos_usu"],
-    "correo" : resultado[0]["correo_usu"],
-    "celular" : resultado[0]["telefono_usu"],
-    "sexo" : resultado[0]["sexo_usu"],
-    "direccion" : resultado[0]["direccion_pac"],
-    "idPlan" : resultado[0]["id_plan"],
-    "plan" : resultado[0]["nombre_plan"],
-    "afiliacionPlan" : resultado[0]["fecha_afiliacion_plan"],
+    "dni": resultado[0]["dni_usu"],
+    "nombres": resultado[0]["nombres_usu"],
+    "apellidos": resultado[0]["apellidos_usu"],
+    "correo": resultado[0]["correo_usu"],
+    "celular": resultado[0]["telefono_usu"],
+    "sexo": resultado[0]["sexo_usu"],
+    "direccion": resultado[0]["direccion_pac"],
+    "idPlan": resultado[0]["id_plan"],
+    "plan": resultado[0]["nombre_plan"],
+    "afiliacionPlan": resultado[0]["fecha_afiliacion_plan"],
   };
 }
 
-void obtenerPersonal(String identificador) async{
-  final resp = await http.post(urlObtenerPersonal, body: {
-    "nro_colegiado": identificador
-  });
+void obtenerPersonal(String identificador) async {
+  final resp = await http
+      .post(urlObtenerPersonal, body: {"nro_colegiado": identificador});
 
   var resultado = jsonDecode(resp.body);
-  
+
   datosPersonales = {
-    "nro" : resultado[0]["nro_colegiado"],
-    "dni" : resultado[0]["dni_den"],
-    "nombres" : resultado[0]["nombres_den"],
-    "apellidos" : resultado[0]["apellidos_den"],
-    "correo" : resultado[0]["correo_den"],
-    "universidad" : resultado[0]["casa_estudio"],
+    "nro": resultado[0]["nro_colegiado"],
+    "dni": resultado[0]["dni_den"],
+    "nombres": resultado[0]["nombres_den"],
+    "apellidos": resultado[0]["apellidos_den"],
+    "correo": resultado[0]["correo_den"],
+    "universidad": resultado[0]["casa_estudio"],
   };
 }
 
-void obtenerAdmin(String identificador) async{
-  final resp = await http.post(urlObtenerAdmin, body: {
-    "id": identificador
-  });
+void obtenerAdmin(String identificador) async {
+  final resp = await http.post(urlObtenerAdmin, body: {"id": identificador});
 
   var resultado = jsonDecode(resp.body);
-  
+
   datosPersonales = {
-    "id" : resultado[0]["id_adm"],
-    "nombres" : resultado[0]["nombres_adm"],
-    "apellidos" : resultado[0]["apellidos_adm"],
-    "correo" : resultado[0]["correo_adm"],
+    "id": resultado[0]["id_adm"],
+    "nombres": resultado[0]["nombres_adm"],
+    "apellidos": resultado[0]["apellidos_adm"],
+    "correo": resultado[0]["correo_adm"],
   };
 }
 
-void obtenerPlanes() async{
+void obtenerPlanes() async {
   final resp = await http.post(urlObtenerPlanes);
 
   var resultado = jsonDecode(resp.body);
-  
+
   datosPlanes.clear();
   datosPlanes = [
     {
-      "id" : resultado[0]["id_plan"],
-      "plan" : resultado[0]["nombre_plan"],
-      "costo" : resultado[0]["costo_plan"],
+      "id": resultado[0]["id_plan"],
+      "plan": resultado[0]["nombre_plan"],
+      "costo": resultado[0]["costo_plan"],
     },
     {
-      "id" : resultado[1]["id_plan"],
-      "plan" : resultado[1]["nombre_plan"],
-      "costo" : resultado[1]["costo_plan"],
+      "id": resultado[1]["id_plan"],
+      "plan": resultado[1]["nombre_plan"],
+      "costo": resultado[1]["costo_plan"],
     },
     {
-      "id" : resultado[2]["id_plan"],
-      "plan" : resultado[2]["nombre_plan"],
-      "costo" : resultado[2]["costo_plan"],
+      "id": resultado[2]["id_plan"],
+      "plan": resultado[2]["nombre_plan"],
+      "costo": resultado[2]["costo_plan"],
     },
   ];
 }
 
-void obtenerCitas() async{
-  final resp = await http.post(urlObtenerCitas, body: {
-    "dni": datosPersonales["dni"]
-  });
+void obtenerCitas() async {
+  final resp =
+      await http.post(urlObtenerCitas, body: {"dni": datosPersonales["dni"]});
 
   var resultado = jsonDecode(resp.body);
 
   datosCitas.clear();
   for (var i = 0; i < resultado.length; i++) {
-    datosCitas.add(
-      {
-        "id" : resultado[i]["id_cita"],
-        "especialidad" : resultado[i]["id_esp"],
-        "generado" : resultado[i]["fecha_gen_cita"],
-        "fecha" : resultado[i]["fecha_cita"],
-        "hora" : resultado[i]["hora_cita"],
-        "estado" : resultado[i]["estado_cita"],
-      }
-    );
+    datosCitas.add({
+      "id": resultado[i]["id_cita"],
+      "especialidad": resultado[i]["id_esp"],
+      "generado": resultado[i]["fecha_gen_cita"],
+      "fecha": resultado[i]["fecha_cita"],
+      "hora": resultado[i]["hora_cita"],
+      "estado": resultado[i]["estado_cita"],
+    });
   }
 }
 
-Future<List> obtenerCitasLista() async{
+Future<List> obtenerCitasLista() async {
   final resp = await http.post(urlObtenerCitasLista);
 
   final respuesta = jsonDecode(resp.body);
   return respuesta;
 }
 
-void obtenerEspecialidades() async{
+void obtenerEspecialidades() async {
   final res = await http.post(urlObtenerEspecialidades);
-  
+
   final u = jsonDecode(res.body);
 
   datosEspecialidades.clear();
   listaEspecialidades.clear();
   for (var i = 0; i < u.length; i++) {
     datosEspecialidades.add({
-      "id" : u[i]["id_esp"],
-      "nombre" : u[i]["nombre_esp"],
-      "descripcion" : u[i]["descripcion_esp"],
+      "id": u[i]["id_esp"],
+      "nombre": u[i]["nombre_esp"],
+      "descripcion": u[i]["descripcion_esp"],
     });
     listaEspecialidades.add(u[i]["nombre_esp"]);
   }
 }
 
-Future<List> obtenerEspecialiadesFuture() async{
+Future<List> obtenerEspecialiadesFuture() async {
   final res = await http.post(urlObtenerEspecialidades);
-  
+
   final u = jsonDecode(res.body);
 
   return u;
 }
 
-Future<List> obtenerCitasFuture() async{
+Future<List> obtenerCitasFuture() async {
   final res = await http.post(urlObtenerEspecialidades);
-  
+
   final u = jsonDecode(res.body);
 
   return u;
 }
-
 
 //Funciones para insertar datos en MySql
 
 //Funcion para convertir usuario en paciente
-void insertarUsuarioPaciente(String dni, String direccion, String contrasena) async{
+void insertarUsuarioPaciente(
+    String dni, String direccion, String contrasena) async {
   http.post(urlInsertarUsuarioPaciente, body: {
     "dni": dni,
     "direccion": direccion,
@@ -225,7 +213,7 @@ void insertarUsuarioPaciente(String dni, String direccion, String contrasena) as
   });
 }
 
-void insertarEspecialidad(String nombre, String descripcion) async{
+void insertarEspecialidad(String nombre, String descripcion) async {
   http.post(urlInsertarEspecialidad, body: {
     "nombre": nombre,
     "descripcion": descripcion,
@@ -234,39 +222,29 @@ void insertarEspecialidad(String nombre, String descripcion) async{
 
 //Funciones para eliminar datos de MySql
 
-void borrarCita(int id) async{
-  http.post(urlBorrarCita, body: {
-    "id_cita": id.toString()
-  });
+void borrarCita(int id) async {
+  http.post(urlBorrarCita, body: {"id_cita": id.toString()});
 }
 
-void borrarEspecialidad(String id) async{
-  http.post(urlBorrarEspecialidad, body: {
-    "id_esp": id
-  });
+void borrarEspecialidad(String id) async {
+  http.post(urlBorrarEspecialidad, body: {"id_esp": id});
 }
 
 //Funcion para borrar a usuario o paciente
-void borrarPacienteUsuario(String id) async{
-  http.post(urlBorrarPacienteUsuario, body: {
-    "dni": id
-  });
+void borrarPacienteUsuario(String id) async {
+  http.post(urlBorrarPacienteUsuario, body: {"dni": id});
 }
-
 
 //Funcion para editar datos de MySql
-void editarEspecialidad(String id, String nombre, String descripcion) async{
+void editarEspecialidad(String id, String nombre, String descripcion) async {
   http.post(urlEditarEspecialidad, body: {
     "id_esp": id,
-    "nombre_esp" : nombre,
-    "descripcion_esp" : descripcion
+    "nombre_esp": nombre,
+    "descripcion_esp": descripcion
   });
 }
 
-void editarActualizarPlan(String id, String dni, String afiliacion) async{
-  http.post(urlActualizarPlan, body: {
-    "id_plan": id,
-    "dni_pac" : dni,
-    "afiliacion" : afiliacion
-  });
+void editarActualizarPlan(String id, String dni, String afiliacion) async {
+  http.post(urlActualizarPlan,
+      body: {"id_plan": id, "dni_pac": dni, "afiliacion": afiliacion});
 }
--- lib/pages/usuarios/login.dart
+++ lib/pages/usuarios/login.dart
@@ -20,55 +20,45 @@ List<String> formHints = [
 TextEditingController controlCorreo = new TextEditingController();
 TextEditingController controlContrasena = new TextEditingController();
 
-var urlLogin = "http://192.168.18.3/PooWeb/login.php";
+var urlLogin = url + "login.php";
 
-Future<List> login(BuildContext context) async{
-
+Future<List> login(BuildContext context) async {
   final response = await http.post(urlLogin, body: {
     "correo": controlCorreo.text,
     "contrasena": controlContrasena.text,
   });
 
-  var resultado =  jsonDecode(response.body);
+  var resultado = jsonDecode(response.body);
 
-  if(resultado[0] == "no existe"){
-    rowAlert("El correo electrónico ingresado no se encuentra registrado.", context);
-  }else if(resultado[0] == "contrasena incorrecta"){
+  if (resultado[0] == "no existe") {
+    rowAlert(
+        "El correo electrónico ingresado no se encuentra registrado.", context);
+  } else if (resultado[0] == "contrasena incorrecta") {
     rowAlert("La contraseña ingresada es incorrecta.", context);
-  }else{
+  } else {
     rolGlobal = resultado[0];
     print("Inicio de sesión exitoso, bienvenido $rolGlobal.");
-    if(resultado[0] == "paciente"){
+    if (resultado[0] == "paciente") {
       obtenerPaciente(resultado[1]);
-      datosPersonales = {
-        "dni" : resultado[1],
-        "nombres" : resultado[2]
-      };
+      datosPersonales = {"dni": resultado[1], "nombres": resultado[2]};
       Navigator.of(context).pushAndRemoveUntil(
-        CupertinoPageRoute(builder: (context) => HomePacientes()),(Route<dynamic> route) => false
-      );
-    }else if(resultado[0] == "personal"){
-      datosPersonales = {
-        "nro" : resultado[1],
-        "nombres" : resultado[2]
-      };
+          CupertinoPageRoute(builder: (context) => HomePacientes()),
+          (Route<dynamic> route) => false);
+    } else if (resultado[0] == "personal") {
+      datosPersonales = {"nro": resultado[1], "nombres": resultado[2]};
       obtenerPersonal(datosPersonales["nro"]);
       Navigator.of(context).pushAndRemoveUntil(
-        CupertinoPageRoute(builder: (context) => HomePacientes()),(Route<dynamic> route) => false
-      );
-    }else if(resultado[0] == "admin"){
+          CupertinoPageRoute(builder: (context) => HomePacientes()),
+          (Route<dynamic> route) => false);
+    } else if (resultado[0] == "admin") {
       obtenerAdmin(resultado[1]);
-      datosPersonales = {
-        "id" : resultado[1],
-        "nombres" : resultado[2]
-      };
+      datosPersonales = {"id": resultado[1], "nombres": resultado[2]};
       Navigator.of(context).pushAndRemoveUntil(
-        CupertinoPageRoute(builder: (context) => HomePacientes()),(Route<dynamic> route) => false
-      );
+          CupertinoPageRoute(builder: (context) => HomePacientes()),
+          (Route<dynamic> route) => false);
     }
   }
 }
-
 
 List<TextEditingController> formControllers = [
   controlCorreo,
@@ -84,31 +74,33 @@ bool bienCorreoForm = true;
 bool bienContrasenaForm = true;
 
 class _LoginState extends State<Login> {
- @override
+  @override
   Widget build(BuildContext context) {
-
     var screenS = MediaQuery.of(context).size;
     var sW = screenS.width;
     var sH = screenS.height;
     double formW = sW > maxScreenSize ? sW : minScreenSize;
-  
-    double width = sW > maxScreenSize ? sW : maxScreenSize + 1/sW;
-    double separador = lerpDouble(10, 16, sW/width);
+
+    double width = sW > maxScreenSize ? sW : maxScreenSize + 1 / sW;
+    double separador = lerpDouble(10, 16, sW / width);
 
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: new Size.fromHeight(sizeAppBar),
-        child: MyRAppBar(tipo: "login")
-      ),
+          preferredSize: new Size.fromHeight(sizeAppBar),
+          child: MyRAppBar(tipo: "login")),
       body: SingleChildScrollView(
         child: Container(
-          height: sW > maxScreenSize ? sizeMiPantalla-sizeAppBar : sizeMiPantalla-sizeAppBar-95,
+          height: sW > maxScreenSize
+              ? sizeMiPantalla - sizeAppBar
+              : sizeMiPantalla - sizeAppBar - 95,
           padding: EdgeInsets.only(top: 60),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Row(
-                mainAxisAlignment: sW > maxScreenSize ? MainAxisAlignment.spaceEvenly : MainAxisAlignment.center,
+                mainAxisAlignment: sW > maxScreenSize
+                    ? MainAxisAlignment.spaceEvenly
+                    : MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   HideIf(
@@ -122,11 +114,10 @@ class _LoginState extends State<Login> {
                           height: sW * 1 / 3,
                           width: sW * 2 / 4,
                           decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: AssetImage("assets/dentista-dibujo.png"),
-                              alignment: Alignment.center
-                            )
-                          ),
+                              image: DecorationImage(
+                                  image:
+                                      AssetImage("assets/dentista-dibujo.png"),
+                                  alignment: Alignment.center)),
                         ),
                       ),
                     ),
@@ -134,52 +125,47 @@ class _LoginState extends State<Login> {
                   HideIf(
                     minWidth: maxScreenSize,
                     child: Container(
-                      margin: EdgeInsets.only(left: sW/50),
-                      width: 1,
-                      height: sH * 4/8,
-                      color: MyColors().colorGris()
-                    ),
+                        margin: EdgeInsets.only(left: sW / 50),
+                        width: 1,
+                        height: sH * 4 / 8,
+                        color: MyColors().colorGris()),
                   ),
                   Container(
-                    width: sW > maxScreenSize ? formW * 1 / 4 : formW * 7/10,
+                    width: sW > maxScreenSize ? formW * 1 / 4 : formW * 7 / 10,
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-
                         MyRText(
-                          text: "Reserva tu cita completamente gratis",
-                          color: MyColors().colorOscuro(),
-                          tipo: "body",
-                          bold: 4
-                        ),
-
+                            text: "Reserva tu cita completamente gratis",
+                            color: MyColors().colorOscuro(),
+                            tipo: "body",
+                            bold: 4),
                         MyRText(
-                          text: "Inicia Sesión",
-                          color: MyColors().colorOscuro(),
-                          tipo: "title",
-                          bold: 7
-                        ),
+                            text: "Inicia Sesión",
+                            color: MyColors().colorOscuro(),
+                            tipo: "title",
+                            bold: 7),
                         Container(height: separador * 0.7),
-
                         _formFields(sW),
-
                         Container(height: separador),
-                        
                         Container(
-                          constraints: BoxConstraints(minWidth: sW > maxScreenSize ? formW * 1 / 4 : formW * 7/10),
+                          constraints: BoxConstraints(
+                              minWidth: sW > maxScreenSize
+                                  ? formW * 1 / 4
+                                  : formW * 7 / 10),
                           child: MyRButton(
                             onPressed: () {
-                              if (controlCorreo.text == ""){
+                              if (controlCorreo.text == "") {
                                 setState(() {
                                   bienCorreoForm = false;
                                   print(bienCorreoForm);
                                 });
-                              }else if (controlContrasena.text == ""){
+                              } else if (controlContrasena.text == "") {
                                 setState(() {
                                   bienContrasenaForm = false;
                                   print(bienContrasenaForm);
                                 });
-                              }else{
+                              } else {
                                 login(context);
                               }
                               controlCorreo.clear();
@@ -193,10 +179,12 @@ class _LoginState extends State<Login> {
                             ),
                           ),
                         ),
-                        Container(height: separador/1.5),
-
+                        Container(height: separador / 1.5),
                         Container(
-                          constraints: BoxConstraints(minWidth: sW > maxScreenSize ? formW * 1 / 4 : formW * 7/10),
+                          constraints: BoxConstraints(
+                              minWidth: sW > maxScreenSize
+                                  ? formW * 1 / 4
+                                  : formW * 7 / 10),
                           child: MyROutlineButton(
                             onPressed: () {},
                             child: MyRText(
@@ -208,53 +196,43 @@ class _LoginState extends State<Login> {
                             color: MyColors().colorOscuro(),
                           ),
                         ),
-                        Container(height: separador* 0.7),
-
+                        Container(height: separador * 0.7),
                         Center(
                           child: TextButton(
                             style: TextButton.styleFrom(
-                              padding: EdgeInsets.symmetric(
-                                horizontal: 15,
-                                vertical: 8
-                              )
-                            ),
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 15, vertical: 8)),
                             child: MyRText(
-                              text: "Olvidé mi contraseña",
-                              tipo: "bodyL",
-                              bold: 5,
-                              color: MyColors().colorGris()
-                            ),
+                                text: "Olvidé mi contraseña",
+                                tipo: "bodyL",
+                                bold: 5,
+                                color: MyColors().colorGris()),
                             onPressed: () {},
                           ),
                         ),
-                        Container(height: separador* 2),
-
+                        Container(height: separador * 2),
                         Row(
                           children: [
                             MyRText(
-                              text: "¿No tienes cuenta?",
-                              tipo: "bodyL",
-                              bold: 5,
-                              color: MyColors().colorGris()
-                            ),
+                                text: "¿No tienes cuenta?",
+                                tipo: "bodyL",
+                                bold: 5,
+                                color: MyColors().colorGris()),
                             SizedBox(width: 5),
                             TextButton(
                               style: TextButton.styleFrom(
-                                padding: EdgeInsets.symmetric(
-                                  horizontal: 15,
-                                  vertical: 8
-                                )
-                              ),
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: 15, vertical: 8)),
                               child: MyRText(
-                                text: "Registrate",
-                                tipo: "bodyL",
-                                bold: 6,
-                                color: MyColors().colorOscuro()
-                              ),
-                              onPressed: (){
-                                
-                                Navigator.pushReplacement(context,
-                                  new CupertinoPageRoute(builder: (context) => Registro()));
+                                  text: "Registrate",
+                                  tipo: "bodyL",
+                                  bold: 6,
+                                  color: MyColors().colorOscuro()),
+                              onPressed: () {
+                                Navigator.pushReplacement(
+                                    context,
+                                    new CupertinoPageRoute(
+                                        builder: (context) => Registro()));
                               },
                             ),
                           ],
@@ -272,54 +250,47 @@ class _LoginState extends State<Login> {
   }
 
   Container _formFields(double sW) {
-
     Color colorNormalForm = MyColors().colorGris();
     Color colorErrorForm = Colors.red;
     Color colorCorreoForm = bienCorreoForm ? colorNormalForm : colorErrorForm;
-    Color colorContrasenaForm = bienContrasenaForm ? colorNormalForm : colorErrorForm;
-  
-    double width = sW > maxScreenSize ? sW : maxScreenSize + 1/sW;
+    Color colorContrasenaForm =
+        bienContrasenaForm ? colorNormalForm : colorErrorForm;
 
-    double separador = lerpDouble(10, 16, sW/width);
+    double width = sW > maxScreenSize ? sW : maxScreenSize + 1 / sW;
+
+    double separador = lerpDouble(10, 16, sW / width);
 
     return Container(
-      child: Column(
-        children: [
-          Container(height: separador),
-
-          MyRTextFormField(
-            textColor: colorCorreoForm,
-            onTap: (){
+        child: Column(children: [
+      Container(height: separador),
+      MyRTextFormField(
+          textColor: colorCorreoForm,
+          onTap: () {
+            setState(() {
               setState(() {
-                setState(() {
-                  bienCorreoForm = true;
-                });
+                bienCorreoForm = true;
               });
-            },
-            hintText: formHints[0],
-            controller: formControllers[0],
-            keyboardType: TextInputType.emailAddress,
-            obscureText: false,
-            formColor: MyColors().colorGrisClaro()
-          ),
-          Container(height: separador),
-
-          MyRTextFormField(
-            textColor: colorContrasenaForm,
-            onTap: () {
-              setState(() {
-                bienContrasenaForm = true;
-              });
-            },
-            hintText: formHints[1],
-            controller: formControllers[1],
-            keyboardType: TextInputType.text,
-            obscureText: true,
-            formColor: MyColors().colorGrisClaro()
-          ),
-          Container(height: separador),
-        ]
-      )
-    );
+            });
+          },
+          hintText: formHints[0],
+          controller: formControllers[0],
+          keyboardType: TextInputType.emailAddress,
+          obscureText: false,
+          formColor: MyColors().colorGrisClaro()),
+      Container(height: separador),
+      MyRTextFormField(
+          textColor: colorContrasenaForm,
+          onTap: () {
+            setState(() {
+              bienContrasenaForm = true;
+            });
+          },
+          hintText: formHints[1],
+          controller: formControllers[1],
+          keyboardType: TextInputType.text,
+          obscureText: true,
+          formColor: MyColors().colorGrisClaro()),
+      Container(height: separador),
+    ]));
   }
 }
